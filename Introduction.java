9a5ac8da80ad8448dbdedeab4636df3fd81215ff
Anjalithurpati <anjalithurpati@gmail.com>
2024-07-18 22:15:32 +0530

    Add files via upload

---
+++ Introduction.java
@@ -0,0 +1,20 @@
+
+public class Introduction {
+
+	public static void main(String[] args) {
+		System.out.println("my name is Anjali"); 
+		System.out.println("I am from Thorrur");
+		System.out.println("I am pursuing MCA in avanthi pg college");
+		System.out.println("I hava completed my graduation in BCOM(COMPUTER APPLICATIONS)");
+		System.out.println("I have completed my inter in MPC");
+		System.out.println("I have completed my ssc ");
+		System.out.println("my father name is srinivas");
+		System.out.println("my mother name is vinodha");
+		System.out.println("my brothers name are shiva and kiran");
+		System.out.println("my sister name is saritha");
+	
+
+
+	}
+
+}
